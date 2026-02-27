651ee5da8d08c0e77324ed5b0f21ad193c17c8d4
Mirro <mirro@zinuo.cc>
2026-02-26 23:06:48 -0500

    Initial Commit

---
+++ MTFHRTTests/MTFHRTTests.swift
@@ -0,0 +1,16 @@
+//
+//  MTFHRTTests.swift
+//  MTFHRTTests
+//
+//  Created by Mirro on 2/26/26.
+//
+
+import Testing
+
+struct MTFHRTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
