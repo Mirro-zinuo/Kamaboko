d7769309455dcd948af64d27d5e89c1752ad3007
Mirro <mirro@zinuo.cc>
2026-03-15 05:24:45 -0400

    Initial Commit

---
+++ KamabokoTests/KamabokoTests.swift
@@ -0,0 +1,16 @@
+//
+//  KamabokoTests.swift
+//  KamabokoTests
+//
+//  Created by Mirro on 3/15/26.
+//
+
+import Testing
+
+struct KamabokoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
